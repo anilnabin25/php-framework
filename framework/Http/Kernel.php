9d262fbd9b1f848d78ca2ba37425c448c75c5664
Divya Shrestha <divyashrestha042@gmail.com>
2024-02-13 17:21:42 +0545

    adding Retrieving Path Info

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,12 +2,32 @@
 
 namespace DivyaShrestha\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request): Response
     {
-        $content = "<h1> Hello world</h1>";
-        return new Response($content);
+        // create a dispatcher
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
+            $routeCollector->addRoute('GET', '/', function () {
+                $content = "<h1> Hello world</h1>";
+                return new Response($content);
+            });
+        });
+
+        // Dispatch a URI, to obtain the route info
+        $routeInfo = $dispatcher->dispatch(
+            $request->getMethod(),
+            $request->getPathInfo()
+        );
+
+        [$status, $handler, $vars] = $routeInfo;
+
+        // call the handler, provided by the route info, in order to create a Response
+        return $handler($vars);
+
 
     }
 }
