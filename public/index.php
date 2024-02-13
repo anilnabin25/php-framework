f369c2083bc419ebb4b42da51cff54c8f562edfc
Divya Shrestha <divyashrestha042@gmail.com>
2024-02-13 16:47:51 +0545

    response class created

--- public/index.php
+++ public/index.php
@@ -1,16 +1,17 @@
 <?php
 declare(strict_types=1);
 use DivyaShrestha\Framework\Http\Request;
+use DivyaShrestha\Framework\Http\Response;
 
 require_once dirname(__DIR__). '/vendor/autoload.php';
 
 // request received
 $request = Request::createFromGlobals();
 
-dd($request);
-
 // perform some logic
 
 // send response (string of content
+$content= "<h1> Hello world</h1>";
 
-echo "hello world";
+$response = new Response(content: $content, status: 200, headers: []);
+$response->send();
